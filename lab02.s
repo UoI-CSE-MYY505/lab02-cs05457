144db66c9cd3d12b49d68635c8c42874b305591b
cs05457 <cs05457@uoi.gr>
2024-10-20 11:06:56 +0300

    Completed Lab02

--- lab02.s
+++ lab02.s
@@ -12,6 +12,21 @@ prog:
 #-----------------------------
 # Write your code here!
 # Do not remove the prog label or write code above it!
+    add s0 , zero, zero
+    add t0 , zero, zero
+loop:
+    beq a1 , t0 ,endloop
+    slli t1 , t0 , 2
+    add t1, t1, a0
+    lw t2 , 0(t1)
+    bne t2,  a2, skip
+    add s0 , t1 , zero
+skip:
+    addi t0 , t0 ,1
+    j loop
+    
+endloop:
+
 #-----------------------------
 done:
     addi a7, zero, 10 
